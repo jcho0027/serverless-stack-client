28777229be07449f45190486f0f0192b6be9e40c
jcho0027 <jcho0027@student.monash.edu>
2020-03-30 10:28:44 +0800

    Build React app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_H3TQUd3ePlYDCQC1Q4lPa1d200ofPZaiyD",
   s3: {
-    REGION: us-east-1,
-    BUCKET: notes-app-uploads
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads"
   },
   apiGateway: {
-    REGION: us-east-1,
-    URL: https://la7fhgzg0m.execute-api.us-east-1.amazonaws.com/prod
+    REGION: "us-east-1",
+    URL: "https://la7fhgzg0m.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: us-east-1,
-    USER_POOL_ID: us-east-1_Nqy3wBXYY,
-    APP_CLIENT_ID: 7i9ik3fdjhg2or1a8p1smrokf3,
-    IDENTITY_POOL_ID: us-east-1:598580b7-cc6b-4453-9e4e-76d52f4223d8
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_Nqy3wBXYY",
+    APP_CLIENT_ID: "7i9ik3fdjhg2or1a8p1smrokf3",
+    IDENTITY_POOL_ID: "us-east-1:598580b7-cc6b-4453-9e4e-76d52f4223d8"
   }
 };
